f8ad442896617ad3494145992c9a5822feb1cd1e
Benoit Jadinon <benoit@skipr.co>
2020-11-04 14:58:57 +0100

    migrated to AndroidX

--- test/flutter_kochava_plugin_test.dart
+++ test/flutter_kochava_plugin_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_kochava_plugin/flutter_kochava_plugin.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_kochava_plugin');
